8b5c1ee5f3e28cb2f7a396f7f60dc972439b44a7
Pavel Ferkl <pavel.ferkl@gmail.com>
2019-07-05 19:32:29 +0200

    more mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ autodoc_default_options = {
 }
 
 # don't import these modules for building the documentation
-autodoc_mock_imports = ['vapory', 'yamlargparse',
+autodoc_mock_imports = ['vapory', 'yamlargparse', 'pandas',
                         'blessings', 'spack', 'vtk', 'gmsh-sdk', 'PyYAML',
                         'munch']
 
